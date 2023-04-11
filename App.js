b4aec4d19e6c681dcad28a18eb5fee975f9c3e27
Julian <julian.h.bonilla@gmail.com>
2023-04-11 16:00:04 -0700

    inital commit

---
+++ App.js
@@ -0,0 +1,110 @@
+import { useState, useEffect, useCallback, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, FlatList, StyleSheet, Text, View } from "react-native";
+import { CheckBox, Input, Button } from "@rneui/themed";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Resistence Trainer" }}
+        />
+        <Stack.Screen name="Registration" component={RegistrationScreen} />
+        <Stack.Screen name="Regiment Selection" component={SelectionScreen} />
+        <Stack.Screen name="Regiment Editor" component={EditingScreen} />
+        <Stack.Screen name="Goals" component={GoalsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function LoginScreen({ navigation }) {
+  return (
+    <>
+      <Text>This is the Login Screen</Text>
+      <Button
+        title="Register"
+        onPress={() => {
+          navigation.navigate("Registration");
+        }}
+      ></Button>
+    </>
+  );
+}
+function RegistrationScreen({ navigation }) {
+  return (
+    <>
+      <Text>This is the Registration Screen</Text>
+      <Button
+        title="Complete Registration"
+        onPress={() => {
+          navigation.navigate("Regiment Selection");
+        }}
+      ></Button>
+    </>
+  );
+}
+
+function SelectionScreen({ navigation }) {
+  return (
+    <>
+      <Text>This is the Workout Selection Screen</Text>
+      <Button
+        title="Edit Regiment"
+        onPress={() => {
+          navigation.navigate("Regiment Editor");
+        }}
+      ></Button>
+      <Button
+        title="Begin Workout"
+        onPress={() => {
+          navigation.navigate("Workout Display");
+        }}
+      ></Button>
+      <Button
+        title="View Goals"
+        onPress={() => {
+          navigation.navigate("Goals");
+        }}
+      ></Button>
+    </>
+  );
+}
+
+function EditingScreen({ navigation }) {
+  return (
+    <>
+      <Text>This is the Regiment Editor Screen</Text>
+      <Button
+        title="Return to Workout Selection"
+        onPress={() => {
+          navigation.navigate("Regiment Selection");
+        }}
+      ></Button>
+    </>
+  );
+}
+
+function GoalsScreen({ navigation }) {
+  return (
+    <>
+      <Text>This is the Regiment Editor Screen</Text>
+      <Button
+        title="Return to Workout Selection"
+        onPress={() => {
+          navigation.navigate("Regiment Selection");
+        }}
+      ></Button>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
