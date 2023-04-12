a311f181d4ecf8aebf30d954930d5d35cd6cdc0e
Julian <julian.h.bonilla@gmail.com>
2023-04-12 11:59:15 -0700

    added login + some reg

--- App.js
+++ App.js
@@ -7,8 +7,27 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
+const loginData = [];
+const defaultUser = {
+  username: "test",
+  password: "Test1@",
+};
 
 export default function App() {
+  useEffect(() => {
+    async function getValue() {
+      const value = await AsyncStorage.getItem("@loginData");
+      if (value === null) {
+        console.log("Storing user data" + JSON.stringify(defaultUser));
+        loginData.push(defaultUser);
+        await AsyncStorage.setItem("@loginData", JSON.stringify(loginData));
+      } else {
+        console.log("Retrieving user data" + value);
+        loginData.push(...JSON.parse(value));
+      }
+    }
+    getValue();
+  }, []);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
@@ -28,12 +47,21 @@ export default function App() {
 }
 
 function LoginScreen({ navigation }) {
+  let [username, setUsername] = useState(" ");
+  let [password, setPassword] = useState(" ");
   return (
     <>
-      <Text>This is the Login Screen</Text>
+      <Input placeholder="Username" onChangeText={setUsername}></Input>
+      <Input placeholder="Password" onChangeText={setPassword}></Input>
       <Button
-        title="Select Regiment"
+        color="#000"
+        title="Login"
+        testID="login-button"
         onPress={() => {
+          console.log(`Username: ${username} Password: ${password}`);
+          // console.log(loginData);
+          // let validUser = (input) => input.username === username;
+          // let validPass = (input) => input.password === password;
           navigation.navigate("Regiment Selection");
         }}
       ></Button>
@@ -49,6 +77,76 @@ function LoginScreen({ navigation }) {
 function RegistrationScreen({ navigation }) {
   return (
     <>
+      <Text style={styles.header}>
+        Please fill out the required fields below.
+      </Text>
+      <Input
+        containerStyle={styles.input}
+        placeholder="First Name"
+        testID="firstname"
+        onChangeText={setFname}
+        name="firstname"
+        errorMessage={validateName(fName)}
+      ></Input>
+      <Input
+        containerStyle={styles.input}
+        placeholder="Last Name"
+        testID="lastname"
+        onChangeText={setLname}
+        errorMessage={validateName(lName)}
+      ></Input>
+      <Input
+        containerStyle={styles.input}
+        placeholder="Username"
+        testID="username"
+        onChangeText={setUsername}
+      ></Input>
+      <Input
+        containerStyle={styles.input}
+        placeholder="Password - Be sure to include an uppercase letter and a number."
+        testID="password"
+        onChangeText={setPassword}
+        errorMessage={validatePassword(password)}
+      ></Input>
+      <Input
+        containerStyle={styles.input}
+        placeholder="Confirm Password"
+        testID="confirmpassword"
+        onChangeText={setConfrimPass}
+        errorMessage={validateConfirmPass(confirmPass)}
+      ></Input>
+      <Input
+        containerStyle={styles.input}
+        placeholder="Email - someone@email.com"
+        testID="email"
+        onChangeText={setEmail}
+        onBlur={() => {
+          let result = validateEmail(email);
+          return result;
+        }}
+        errorMessage={validateEmail(email)}
+      ></Input>
+      <Button
+        style={styles.button}
+        color="#000"
+        title="Sign Up"
+        testID="register-button"
+        onPress={() => {
+          loginData.push(newUser);
+          for (var i = 0; i < flags.length; i++) {
+            console.log(flags);
+            if (flags[i] == true) {
+              gate = gate + 1;
+            }
+          }
+          console.log(gate);
+          if (gate != 5) {
+            alert("All fields must be filled in correctly.");
+          } else {
+            navigation.navigate("Todo Screen");
+          }
+        }}
+      ></Button>
       <Text>This is the Registration Screen</Text>
       <Button
         title="Complete Registration"
